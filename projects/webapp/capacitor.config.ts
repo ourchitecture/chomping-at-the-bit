e2b09feb5af0f678af2c3962842b0ea61f18482f
Eric Swanson <ericis@users.noreply.github.com>
2022-03-09 17:47:30 -0500

    feat: web app skeleton
    Signed-off-by: Eric Swanson <ericis@users.noreply.github.com>

---
+++ projects/webapp/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'bit-chomping',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
