0dcf53ae7671daa721dcf7d2e5166238d8573805
ericis <ericis@users.noreply.github.com>
2022-04-08 22:11:34 +0000

    docs: auto-generated documentation [skip ci]
    Signed-off-by: GitHub <noreply@github.com>

--- docs/js/78.ff0c116a.js
+++ docs/js/78.ff0c116a.js
@@ -3,4 +3,4 @@
  * (C) Ionic http://ionicframework.com - MIT License
  */
 const s=()=>{const t=window;t.addEventListener("statusTap",(()=>{(0,o.wj)((()=>{const e=t.innerWidth,n=t.innerHeight,s=document.elementFromPoint(e/2,n/2);if(!s)return;const c=s.closest("ion-content");c&&new Promise((t=>(0,r.c)(c,t))).then((()=>{(0,o.Iu)((async()=>{c.style.setProperty("--overflow","hidden"),await c.scrollToTop(300),c.style.removeProperty("--overflow")}))}))}))}))}}}]);
-//# sourceMappingURL=78.037f4c64.js.map
+//# sourceMappingURL=78.ff0c116a.js.map
