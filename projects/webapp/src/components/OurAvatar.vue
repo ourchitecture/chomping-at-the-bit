4ff3d48d5c33c24e5637517e924fa87e5e92e118
Eric Swanson <ericis@users.noreply.github.com>
2022-04-30 09:03:22 -0400

    feat: expertise
    Signed-off-by: Eric Swanson <ericis@users.noreply.github.com>

---
+++ projects/webapp/src/components/OurAvatar.vue
@@ -0,0 +1,91 @@
+<template>
+  <ion-card>
+    <ion-card-header>
+      <ion-card-subtitle color="secondary">
+        {{ avatar.position }}
+      </ion-card-subtitle>
+      <ion-card-title color="primary">
+        {{ avatar.name }}
+      </ion-card-title>
+    </ion-card-header>
+    <ion-card-content>
+      <div class="clearfix">
+        <div><component v-bind:is="avatar.component"></component></div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pulvinar, diam sed vulputate ullamcorper, metus.
+        </p>
+      </div>
+      <ion-list>
+        <ion-item>
+          <ion-label>Business Expertise</ion-label>
+          <ion-badge slot="end">{{ avatar.expertise.business }}</ion-badge>
+        </ion-item>
+        <ion-item>
+          <ion-label>People Expertise</ion-label>
+          <ion-badge slot="end">{{ avatar.expertise.people }}</ion-badge>
+        </ion-item>
+        <ion-item>
+          <ion-label>Process Expertise</ion-label>
+          <ion-badge slot="end">{{ avatar.expertise.process }}</ion-badge>
+        </ion-item>
+        <ion-item>
+          <ion-label>Technology Expertise</ion-label>
+          <ion-badge slot="end">{{ avatar.expertise.technology }}</ion-badge>
+        </ion-item>
+      </ion-list>
+    </ion-card-content>
+  </ion-card>
+</template>
+
+<script lang="ts">
+import {
+  IonBadge,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonItem,
+  IonLabel,
+  IonList,
+} from '@ionic/vue';
+
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  props: ['avatar'],
+  components: {
+    IonBadge,
+    IonCard,
+    IonCardContent,
+    IonCardHeader,
+    IonCardSubtitle,
+    IonCardTitle,
+    IonItem,
+    IonLabel,
+    IonList,
+  },
+});
+</script>
+
+<style scoped>
+ion-card {
+  display: inline-block;
+  flex-wrap: wrap;
+  height: 100%;
+  width: 300px;
+}
+
+.our-avatar {
+  max-height: 150px;
+  float: left;
+  padding-right: 15px;
+  padding-bottom: 15px;
+}
+
+.clearfix::after {
+  display: block;
+  clear: both;
+  content: "";
+}
+</style>
--- projects/webapp/src/stores/avatarStore.ts
+++ projects/webapp/src/stores/avatarStore.ts
@@ -1,11 +1,11 @@
 import { defineStore } from 'pinia';
 
 import {
+  IOrgChartEmployee,
   genderMap,
   expertiseTitles,
   generateAvatarComponents,
   generateRandomEmployee,
-  generateRandomName,
 } from './avatars';
 
 export { GenderEnum } from './avatars';
@@ -15,6 +15,25 @@ export const useAvatarStore = defineStore('AvatarStore', {
 
     const avatars = generateAvatarComponents();
 
+    const randomEmployees: IOrgChartEmployee[] = [];
+
+    while (randomEmployees.length < 6) {
+      let randomEmployee = generateRandomEmployee(avatars);
+
+      // avoid reusing an employee id (i.e. same avatar)
+      while (randomEmployees.find((employee: IOrgChartEmployee) => employee.id == randomEmployee.id)) {
+        randomEmployee = generateRandomEmployee(avatars);
+      }
+
+      randomEmployees.push(randomEmployee);
+    }
+
+    const orgChart = randomEmployees[0];
+
+    const directReports = randomEmployees.slice(1);
+
+    orgChart.directReports.push(...directReports);
+
     return {
       // multiple genders are indeed recognized, however the avatars represent a "preference" too
       // setting this flag to `true` will ensure that ALL avatars are gender-neutral including
@@ -23,14 +42,7 @@ export const useAvatarStore = defineStore('AvatarStore', {
       genderMap,
       expertiseTitles,
       avatars,
+      orgChart,
     };
-  },
-  getters: {
-    getRandomName: () => {
-      return generateRandomName;
-    },
-    createRandomEmployee: (state) => {
-      return () => generateRandomEmployee(state.avatars);
-    }
   },
 });
